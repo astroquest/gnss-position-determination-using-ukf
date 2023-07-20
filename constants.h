a7478c69791a27b632e23983fc222e22259c2576
astroquest <maartenmenken98@gmail.com>
2023-07-20 22:27:34 +0200

    first satellite simulation stuff

--- constants.h
+++ constants.h
@@ -2,4 +2,5 @@
 double c = 3e8;
 double pi = 3.1415926;
 double mu = 3.986e14;
-double Re = 6371e3;
+double earth_radius = 6371e3;
+double earth_radial_velocity = 2*pi/(24*60*60)
--- main.cpp
+++ main.cpp
@@ -2,12 +2,20 @@
 #include <Eigen/Dense>
 
 #include "constants.h"
+#include "satellite.h"
 #include "kalman_settings.h"
 #include "covariances.h"
 
 using namespace std;
 
 int main(){
+    double orbital_radius = 25e6;
+
+    Satellite satellite_1(orbital_radius);
+    satellite_1.setInitialConditions(0);
+    satellite_1.propagateOrbit(3600);
+
+
     KalmanSettings kalman_settings;
     Covariances covariances;
 
