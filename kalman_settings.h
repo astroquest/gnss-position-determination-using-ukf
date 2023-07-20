dfe4aca586f92513c4d3709ce8492dcc0dec95a9
astroquest <maartenmenken98@gmail.com>
2023-07-20 14:10:58 +0200

    better class setup

--- kalman_settings.h
+++ kalman_settings.h
@@ -2,21 +2,34 @@ using namespace std;
 
 class KalmanSettings{
     public:
-        void set_constants(int n, double alpha){
+        KalmanSettings(){
+            n = 3;
+            alpha = 1e-3;
             beta = 2;
             lambda = n*(pow(alpha,2) - 1);
             eta = sqrt(n + lambda);
+
+            Wm.resize(2*n+1);
+            Wc.resize(2*n+1);
         };
 
-        void set_weights(int n, double alpha, double beta, double lambda){
+        void set_weights(){
+            Wm[0] = lambda/(n + lambda);
+            Wc[0] = lambda/(n + lambda) + (1 - pow(alpha,2) + beta);
 
+            for(int i = 1; i < 2*n+1; i++){
+                Wm[i] = 1/(2*(n + lambda));
+                Wc[i] = 1/(2*(n + lambda));
+            }
         };
 
         double eta;
-        double Wm[1];
-        double Wc[1];
+        vector<double> Wm;
+        vector<double> Wc;
 
     private:
+        int n;
+        double alpha;
         double beta;
         double lambda;
 };
--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <string.h>
 #include "constants.h"
 #include "kalman_settings.h"
 
 using namespace std;
 
 int main(){
-
     KalmanSettings kalman_settings_1;
+
+    // cout << to_string(kalman_settings_1.set_weights()) << endl;
 
     return 0;
 }
