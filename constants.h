0eabb3edaa152ffd755975967ba1dbd488a5a4ac
astroquest <maartenmenken98@gmail.com>
2023-07-25 12:40:09 +0200

    const in constants

--- constants.h
+++ constants.h
@@ -1,5 +1,5 @@
-double c = 3e8;
-double pi = 3.1415926;
-double mu = 3.986e14;
-double earth_radius = 6371e3;
-double earth_radial_velocity = 2*pi/(24*60*60);
+const double c = 3e8;
+const double pi = 3.1415926;
+const double mu = 3.986e14;
+const double earth_radius = 6371e3;
+const double earth_radial_velocity = 2*pi/(24*60*60);
